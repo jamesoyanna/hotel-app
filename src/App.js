46d0375bcdb20306a41ae3590e5665fb9dc8678f
eshoiza <engrjayt200@gmail.com>
2020-01-10 11:22:11 +0100

    added Navbar & logo

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 import {Route, Switch} from 'react-router-dom';
+import Navbar from './components/Navbar';
 //import './App.css';
 
 function App() {
   return (
     <>
+       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms" component={Rooms} />
