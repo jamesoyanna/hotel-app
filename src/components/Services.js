d69ae75bcd064ae2d7b928d549f0ac2eb3466503
eshoiza <engrjayt200@gmail.com>
2020-01-11 16:17:02 +0100

    added service title

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import {FaCocktail, FaHiking, FaShuttleVan, FaBeer} from 'react-icons/fa';
 export class Services extends Component {
+state = {
+    services:[
+     {
+         icon: <FaCocktail />,
+         title: "Free Cocktail",
+         info: "Loremum ipsum text ready for the new cocktail"
+     },
+     {
+         icon: <FaHiking />,
+         title: "Endless Hiking",
+         info: "Loeum ipsum test.going on a hiking mood now"
+     },
+     {
+         icon: <FaShuttleVan />,
+         title: "Free shuttle",
+         info:
+         "Get the best ride on van round the town. it affordable" 
+     },
+
+     {
+         icon: <FaBeer />,
+         title: "Strongest Beer",
+         info:
+         "Loreump ipsum detty december. Get the best drink"
+     }
+    ]
+}
     render() {
         return (
-            <div>
+            <section className="section">
               <Title title="Services" />
-            </div>
+              <div className="services-center">
+                  
+              </div>
+            </section>
         );
     }
 }
