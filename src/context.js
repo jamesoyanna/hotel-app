0eb92a27b7aa467d9c1b917376f4e60de117dd7a
eshoiza <engrjayt200@gmail.com>
2020-01-18 09:57:43 +0100

    added content for single room

--- src/context.js
+++ src/context.js
@@ -21,22 +21,27 @@ class RoomProvider extends Component {
     });
   }
 
-  formatData(items){
-  let tempItems = items.map(item=>{
-      let id = item.sys.id
-      let images = item.fields.images.map(image=>
-      image.fields.file.url)
-      let room = {...item.fields,images, id}
-      return room
-  });
-  
+  formatData(items) {
+    let tempItems = items.map(item => {
+      let id = item.sys.id;
+      let images = item.fields.images.map(image => image.fields.file.url);
+      let room = { ...item.fields, images, id };
+      return room;
+    });
 
     return tempItems;
   }
 
+  //get Data
+  getRoom = slug => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find(room=>room.slug===slug);
+    return room
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom:this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
