1d94b3c97ffe0671afabedf575a16be091fd559c
eshoiza <engrjayt200@gmail.com>
2020-01-18 16:56:03 +0100

    new commit

---
+++ src/components/StyledHero.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+const simpleButton = styled.button`
+color: blue;
+background: orange;
+`
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -20,13 +20,25 @@ static contextType = RoomContext;
     render() {
 const { getRoom } = this.context;
 const room = getRoom(this.state.slug);
-console.log(room);
+if(!room){
+    return <div className="error">
+        <h3>No such room could be found</h3>
+        <Link to="/rooms" className="btn-primary">
+            Back to room
+        </Link>
+    </div>
+}
 
+const {name, description, capacity,size, price, extras,breakfast,pets, images} = room
 
         return (
-            <div>
-                Hello from single room {room.name}
-            </div>
+         <Hero hero="roomsHero">
+             <Banner title={`${name}room`}>
+                 <Link to="/rooms"className="btn-primary">
+                     Back to rooms  
+                 </Link>
+             </Banner>
+         </Hero>
         );
     }
 }
