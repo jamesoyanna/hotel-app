c8cd5cabd509d2362f41d0696ab6eb47819d4cdb
eshoiza <engrjayt200@gmail.com>
2020-02-04 04:08:25 +0100

    used syled component to access images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,25 +4,20 @@ import Banner from '../components/Banner';
 import { Link }  from 'react-router-dom';
 import Services from '../components/Services';
 import FeaturedRooms from '../components/FeaturedRooms';
-import StyledHero from "../components/StyledHero";
+//import Button from "../components/StyledHero";
 const Home = () => {
-    return (
-      <>
-        <Hero>
-          <Banner
-            title="Luxurious Rooms"
-            subtitle="Deluxe rooms starts at N299"
-          >
+    return <>
+<Hero>
+        <Banner title="Luxurious Rooms" subtitle="Deluxe rooms starts at N299">
             <Link to="/rooms" className="btn-primary">
-              our rooms
+                our rooms
             </Link>
-          </Banner>
-        </Hero>
-        <Services />
-        <FeaturedRooms />
-        <StyledHero />
-      </>
-    );
+        </Banner>
+    </Hero>
+    < Services />
+    <FeaturedRooms />
+    
+    </>
 
 }
 export default Home;
