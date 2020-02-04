aa3ea41f31935702da5c542a6063bf83f4e6eb81
eshoiza <engrjayt200@gmail.com>
2020-02-04 04:38:00 +0100

    added room sizes

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -33,31 +33,32 @@ if(!room){
 const {name, description, capacity,size, price, extras,breakfast,pets, images} = room
 const [mainImg, ...defaultImg] = images
         return (
-            <>
-          <StyledHero img={mainImg || this.state.defaultBcg}>
-            <Banner title={`${name}room`}>
-              <Link to="/rooms" className="btn-primary">
-                Back to rooms
-              </Link>
-            </Banner>
-          </StyledHero>
-          <section className="single-room">
+          <>
+            <StyledHero img={mainImg || this.state.defaultBcg}>
+              <Banner title={`${name}room`}>
+                <Link to="/rooms" className="btn-primary">
+                  Back to rooms
+                </Link>
+              </Banner>
+            </StyledHero>
+            <section className="single-room">
               <div className="single-room-images">
-                  {defaultImg.map((item, index)=>{
-                      return <img key={index} src={item} alt={name}/>
-                  })}
+                {defaultImg.map((item, index) => {
+                  return <img key={index} src={item} alt={name} />;
+                })}
               </div>
               <div className="single-room-info">
-                  <article className="desc">
-                      <h3>Details</h3>
-                      <p>{description}</p>
-                  </article>
-                  <article className="info">
-                      <h3>Info</h3>
-                      <h6>Price: ${price}</h6>
-                  </article>
+                <article className="desc">
+                  <h3>Details</h3>
+                  <p>{description}</p>
+                </article>
+                <article className="info">
+                  <h3>Info</h3>
+                  <h6>Price: ${price}</h6>
+                  <h6>Size: {size}SQFT</h6>
+                </article>
               </div>
-          </section>
+            </section>
           </>
         );
     }
