dea06bdc2a5af50633435d940637619cd9d9bc7f
eshoiza <engrjayt200@gmail.com>
2020-02-22 12:52:45 +0100

    use consumer from context API

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import RoomsFilter from "../components/RoomsFilter";
 import RoomsList from "../components/RoomsList";
+import {RoomConsumer} from "../context";
+import Loading from "../components/Loading";
 
 const RoomsContainer = () => {
+
     return (
-        <div>
-            Hello from  room container
-            <RoomsFilter />
-            <RoomsList />
-        </div>
+
+    <RoomConsumer>
+        {
+            (value)=>{
+                console.log(value);
+
+                return (
+                  <div>
+                    Hello from room container
+                    <RoomsFilter />
+                    <RoomsList />
+                  </div>
+                );
+            }
+        }
+    </RoomConsumer>
+
     );
 }
 
