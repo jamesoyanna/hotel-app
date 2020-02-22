e1960125aef6856e02aa30dff1199a4d8df5ec58
eshoiza <engrjayt200@gmail.com>
2020-02-22 17:50:46 +0100

    aded more items to the setState

--- src/context.js
+++ src/context.js
@@ -13,11 +13,24 @@ class RoomProvider extends Component {
   componentDidMount() {
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter(room => room.featured === true);
+    let maxPrice = Math.max(...rooms.map(item=>item.price));
+    let maxSize = Math.max(...rooms.map(item => item.size));
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
-      loading: false
+      loading: false,
+      type: 'All',
+      capacity: 1,
+      price: 0,
+      minPrice: 0,
+      maxPrice: 0,
+      minSize: 0,
+      maxSize: 0,
+      breakfast: false,
+      pets: false
+
+
     });
   }
 
