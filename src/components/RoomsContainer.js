9c5e75974cb4e66c00b5e3afdb36832502237374
eshoiza <engrjayt200@gmail.com>
2020-02-22 14:55:13 +0100

    updated roms component

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,30 +1,65 @@
+
 import React from 'react';
 import RoomsFilter from "../components/RoomsFilter";
 import RoomsList from "../components/RoomsList";
-import {RoomConsumer} from "../context";
+import {withRoomConsumer} from "../context";
 import Loading from "../components/Loading";
 
-const RoomsContainer = () => {
 
-    return (
-
-    <RoomConsumer>
-        {
-            (value)=>{
-                console.log(value);
-
+function RoomContainer({context}){
+    const {loading, sortedRooms, rooms} = context;
+if(loading){
+               return <Loading />
+           }
                 return (
-                  <div>
-                    Hello from room container
-                    <RoomsFilter />
-                    <RoomsList />
-                  </div>
+                  <>
+                    <RoomsFilter rooms = {rooms} />
+                    <RoomsList rooms= {sortedRooms} />
+                  </>
                 );
             }
-        }
-    </RoomConsumer>
+        
 
-    );
-}
 
-export default RoomsContainer;
+export default withRoomConsumer(RoomContainer);
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import RoomsFilter from "../components/RoomsFilter";
+// import RoomsList from "../components/RoomsList";
+// import {RoomConsumer} from "../context";
+// import Loading from "../components/Loading";
+
+// const RoomsContainer = () => {
+
+//     return (
+
+//     <RoomConsumer>
+//         {
+//             (value)=>{
+//                 const { loading, sortedRooms, rooms} = value
+//            if(loading){
+//                return <Loading />
+//            }
+//                 return (
+//                   <div>
+//                     Hello from room container
+//                     <RoomsFilter rooms = {rooms} />
+//                     <RoomsList rooms= {sortedRooms} />
+//                   </div>
+//                 );
+//             }
+//         }
+//     </RoomConsumer>
+
+//     );
+// }
+
+// export default RoomsContainer;
