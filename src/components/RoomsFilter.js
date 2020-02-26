8b049cb950393fea16476771f8898b95567254e7
eshoiza <engrjayt200@gmail.com>
2020-02-27 00:38:04 +0100

    added a title 'search rooms'

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import  {useContext} from "react";
+import {RoomContext} from "../context";
+import Title from "../components/Title";
 
 const RoomsFilter = () => {
+const context = useContext(RoomContext);
+
+const {handleChange,
+type,
+capacity,
+price,
+minPrice,
+maxPrice,
+minSize,
+maxSize,
+breakfast,
+pets
+}  = context;
+
+
     return (
-        <div>
-            Hello from room filter
-        </div>
+        <section className="filter-container">
+         <Title title="search rooms" />  
+         <form className="filter-form">
+        
+        </form> 
+        </section >
     );
 }
 
